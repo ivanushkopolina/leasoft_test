80de204b4714aed188a6b50a3a43c32c46a100b5
Chamomilla2205 <otamah1995@gmail.com>
2026-02-12 15:39:40 +0200

    feat: added few more tests for Wild

--- src/slot/utils/ways.evaluator.spec.ts
+++ src/slot/utils/ways.evaluator.spec.ts
@@ -68,4 +68,37 @@ describe('evaluateWays', () => {
     const aceWin = result.wins.find((w) => w.symbol === SYMBOL_OBJ.Ace);
     expect(aceWin).toBeUndefined();
   });
+
+  it('Wild should substitute for bigger symbol if wild is after the bigger symbol', () => {
+    const grid: Grid = [
+      [SYMBOL_OBJ.Wild, SYMBOL_OBJ.Ten, SYMBOL_OBJ.Wild, SYMBOL_OBJ.Ace, SYMBOL_OBJ.Ten],
+      [SYMBOL_OBJ.Ten, SYMBOL_OBJ.Queen, SYMBOL_OBJ.Ace, SYMBOL_OBJ.Ace, SYMBOL_OBJ.Ten],
+      [SYMBOL_OBJ.Ace, SYMBOL_OBJ.Ace, SYMBOL_OBJ.Jack, SYMBOL_OBJ.Jack, SYMBOL_OBJ.Scatter],
+    ];
+    const result = evaluateWays(grid, defaultBet);
+    console.log(result.wins);
+    expect(result.wins.length).toBeGreaterThan(0);
+
+    const aceWin = result.wins.find((w) => w.symbol === SYMBOL_OBJ.Ace && w.count === 4);
+    expect(aceWin).toBeDefined();
+    expect(aceWin!.amount).toBe(PAYTABLE[SYMBOL_OBJ.Ace][4]);
+
+    expect(result.totalWin).toBeGreaterThan(0);
+  });
+
+  it('Wild should substitute for bigger symbol if wild is before the bigger symbol', () => {
+    const grid: Grid = [
+      [SYMBOL_OBJ.Wild, SYMBOL_OBJ.Ten, SYMBOL_OBJ.Ten, SYMBOL_OBJ.Ace, SYMBOL_OBJ.Ten],
+      [SYMBOL_OBJ.Ten, SYMBOL_OBJ.Queen, SYMBOL_OBJ.Ace, SYMBOL_OBJ.Jack, SYMBOL_OBJ.Ten],
+      [SYMBOL_OBJ.Ace, SYMBOL_OBJ.Ace, SYMBOL_OBJ.Jack, SYMBOL_OBJ.Jack, SYMBOL_OBJ.Scatter],
+    ];
+    const result = evaluateWays(grid, defaultBet);
+    expect(result.wins.length).toBeGreaterThan(0);
+    console.log(result.wins);
+    const aceWin = result.wins.find((w) => w.symbol === SYMBOL_OBJ.Ace && w.count === 4);
+    expect(aceWin).toBeDefined();
+    expect(aceWin!.amount).toBe(PAYTABLE[SYMBOL_OBJ.Ace][4]);
+
+    expect(result.totalWin).toBeGreaterThan(0);
+  });
 });
